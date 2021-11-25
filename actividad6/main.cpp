7798cbf13eea304b0c35a0448770cfd0c1371b45
Ernesto Miranda <ernomitrovic@zohomail.com>
2021-11-25 15:58:41 -0600

    Test cases programados. Agregue un delegating constructor (revisar).

--- actividad6/main.cpp
+++ actividad6/main.cpp
@@ -1,10 +1,94 @@
 #include "Triangle.hpp"
 #include <iostream>
 
-int main(){
+/*
+Caso 1:
+- Crear triangulo con constructor por defecto
+- Imprimir valores del triangulo
+
+Caso 2: 
+- Crear triangulo con valores introducidos por el usuario
+- Imprimir valores del usuario
+
+Caso 3:
+- Crear vertices colineales
+- Imprimir el resultado
+
+Caso 4:
+- Crear 2 vertices coincidentes
+- Imprimir el resultado
+
+*/
+
+/**
+ * @brief Inicializa e imprime un triangulo por defecto
+ * 
+ */
+void casoUno(){
     Triangle def;
-    Triangle user (Point(-1, -1), Point(30, 40), Point(30, -5));
-    
     def.printTriangle();
+}
+
+/**
+ * @brief Inicializa e imprime un triangulo del usuario
+ * 
+ */
+void casoDos(){
+    Point puntos [3];
+    for(int i = 0; i < 3; i++){
+        float x, y;
+        std::cout << "Vertice " << i+1 << " x: ";
+        std::cin >> x;
+        std::cout << "Vertice " << i+1 << " y: ";
+        std::cin >> y;
+        puntos[i] = Point(x, y);
+    }
+    Triangle user(puntos[0], puntos[1], puntos[2]);
     user.printTriangle();
+}
+
+/**
+ * @brief Test case para un puntos colineales
+ * 
+ */
+void casoTres(){
+    Triangle tColineal(Point(2,5), Point(2, 1), Point(2, 10));
+    tColineal.printTriangle();
+}
+
+/**
+ * @brief Test case para puntos coincidentes
+ * 
+ */
+void casoCuatro(){
+    Triangle tCoincidente(Point(2, 5), Point(2, 5), Point(8, 4));
+    tCoincidente.printTriangle();
+}
+
+void ejecucion(){
+    int opcion;
+    std::cout << "Ingresa el test case a analizar (1-4): ";
+    do {
+        std::cin >> opcion;
+        switch (opcion){
+            case 1:
+                casoUno();
+                break;
+            case 2: 
+                casoDos();
+                break;
+            case 3:
+                casoTres();
+                break;
+            case 4:
+                casoCuatro();
+                break;
+            default:
+                std::cout << "Invalido\n";
+                break;
+        }
+    } while(opcion < 1 || opcion > 4);
+}
+int main(){
+    ejecucion();
 }
--- actividad6/triangle.hpp
+++ actividad6/triangle.hpp
@@ -5,6 +5,8 @@ class Triangle
 {
 private:
     Point vertex1, vertex2, vertex3; // Podria ser un array
+    bool colineal(Point , Point, Point);
+    bool coincidente(Point, Point, Point);
 public:
     Triangle();
     Triangle(Point vertex1, Point vertex2, Point vertex3);
@@ -28,13 +30,60 @@ Triangle::Triangle(){
 }
 
 /**
- * @brief Construct a new Triangle::Triangle object
+ * @brief Revisa si los puntos son colineales
  * 
- * @param v1 vertice 1
- * @param v2 vertice 2
- * @param v3 vertice 3
+ * @param vertex1 
+ * @param vertex2 
+ * @param vertex3 
+ * @return true si todos los puntos son colineales
+ * @return false si no hay puntos colineales o maximo 2
  */
-Triangle::Triangle(Point v1, Point v2, Point v3) : vertex1(v1), vertex2(v2), vertex3(v3) {}
+bool Triangle::colineal(Point vertex1, Point vertex2, Point vertex3){
+    if(vertex1.getX() == vertex2.getX() && vertex2.getX() == vertex3.getX()){
+        return true;
+    } else if(vertex1.getY() == vertex2.getY() && vertex2.getY() == vertex3.getY()){
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief Revisa si al menos 2 de los puntos son coincidentes
+ * 
+ * @param vertex1 
+ * @param vertex2 
+ * @param vertex3 
+ * @return true si 2 de los puntos son coincidentes
+ * @return false si ningun punto es coincidente
+ */
+bool Triangle::coincidente(Point vertex1, Point vertex2, Point vertex3){
+    if (vertex1.getX() == vertex2.getX() && vertex1.getY() == vertex2.getY()){
+        return true;
+    } else if(vertex2.getX() == vertex3.getX() && vertex2.getY() == vertex3.getY()){
+        return true;
+    } else if (vertex1.getX() == vertex3.getX() && vertex1.getY() == vertex3.getY()){
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief Construye un triangulo si los puntos no son colineales o al menos dos no son coincidentes
+ * 
+ * @param vertex1 
+ * @param vertex2 
+ * @param vertex3 
+ */
+Triangle::Triangle(Point vertex1, Point vertex2, Point vertex3) : Triangle::Triangle(){
+    if (!colineal(vertex1, vertex2, vertex3) && !coincidente(vertex1, vertex2, vertex3)){
+        this-> vertex1 = vertex1;
+        this-> vertex2 = vertex2;
+        this-> vertex3 = vertex3;
+    } else{
+        std::cout << "Triangulo invalido, declarando con constructor por defecto...\n";
+        
+    }
+}
 
 /**
  * @brief Calcula el perimetro del triangulo
@@ -63,11 +112,15 @@ double Triangle::area(){
     return std::sqrt(s*(s-a)*(s-b)*(s-c));
 }
 
+/**
+ * @brief Imprime los valores del triangulo
+ * 
+ */
 void Triangle::printTriangle(){
     std::cout << "Triangle vertices:\n" 
     << "1.- (" << vertex1.getX() << ", " << vertex1.getY() << ")\n"
     << "2.- (" << vertex2.getX() << ", " << vertex2.getY() << ")\n"
     << "3.- (" << vertex3.getX() << ", " << vertex3.getY() << ")\n"
-    << "Perimetro: " << perimeter() << "u\n"
-    << "Area: " << area() << 'u' << (char) 253 << std::endl;
+    << "Perimetro: " << perimeter() << " u\n"
+    << "Area: " << area() << " u" << (char) 253 << std::endl;
 }
